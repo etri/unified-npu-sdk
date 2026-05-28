87eeb3f87775d4fb63edada02afb053b22587d75
Jusungkang <k92492@etri.re.kr>
2026-05-28 16:05:26 +0900

    Simplify branch workspace to README only

--- src/unified_sdk/frontends/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-unified_sdk.frontends
----------------------
-Frontend modules for importing and converting models from various frameworks
-(PyTorch, TensorFlow, etc.) into an intermediate format such as ONNX.
-"""
-
